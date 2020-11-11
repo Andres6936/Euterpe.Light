dadf7b905dd29bdc8ad6ca05f872339db99a36ed
Joan Andrés <andres6936@live.com>
2020-11-11 13:39:21 -0500

    Implemented bit rate index for the Layer 3 class

---
+++ src/javazoom/jlme/decoder/Layer3.java
@@ -0,0 +1,39 @@
+package javazoom.jlme.decoder;
+
+import java.util.OptionalInt;
+
+public class Layer3 {
+    public static OptionalInt getBitRateIndex(final int bits) throws IllegalArgumentException {
+        if (bits == 0b0001) {
+            return OptionalInt.of(32);
+        } else if (bits == 0b0010) {
+            return OptionalInt.of(40);
+        } else if (bits == 0b0011) {
+            return OptionalInt.of(48);
+        } else if (bits == 0b0100) {
+            return OptionalInt.of(56);
+        } else if (bits == 0b0101) {
+            return OptionalInt.of(64);
+        } else if (bits == 0b0110) {
+            return OptionalInt.of(80);
+        } else if (bits == 0b0111) {
+            return OptionalInt.of(96);
+        } else if (bits == 0b1000) {
+            return OptionalInt.of(112);
+        } else if (bits == 0b1001) {
+            return OptionalInt.of(128);
+        } else if (bits == 0b1010) {
+            return OptionalInt.of(160);
+        } else if (bits == 0b1011) {
+            return OptionalInt.of(192);
+        } else if (bits == 0b1100) {
+            return OptionalInt.of(224);
+        } else if (bits == 0b1101) {
+            return OptionalInt.of(256);
+        } else if (bits == 0b1110) {
+            return OptionalInt.of(320);
+        }
+
+        throw new IllegalArgumentException("Sequence of bits not is recognized for the standard");
+    }
+}
