2fdb2e0055f426d7952e058b2fdb95a4621f8b97
Joan Andrés <andres6936@live.com>
2020-11-22 17:24:08 -0500

    Added documentation to term "reserved"

--- src/javazoom/jlme/decoder/Layer.java
+++ src/javazoom/jlme/decoder/Layer.java
@@ -3,6 +3,10 @@ package javazoom.jlme.decoder;
 /**
  * One of the levels in the coding hierarchy of the audio system defined in
  * this part of the CD.
+ *
+ * @implNote The term "reserved" when used in the clauses defining the coded
+ * bit stream indicates that the value may be used in the future for ISO/IEC
+ * defined extensions.
  */
 public enum Layer {
     LAYER1,
