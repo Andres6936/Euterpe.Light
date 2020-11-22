b3ee42d559c5ae74f7cd78503385167815d5bd97
Joan Andrés <andres6936@live.com>
2020-11-22 17:10:57 -0500

    Deleted redundant code, functions and const

--- src/javazoom/jlme/decoder/Header.java
+++ src/javazoom/jlme/decoder/Header.java
@@ -1,4 +1,4 @@
-/***************************************************************************
+/*
  *  JLayerME is a JAVA library that decodes/plays/converts MPEG 1/2 Layer 3.
  *  Project Homepage: http://www.javazoom.net/javalayer/javalayerme.html.
  *  Copyright (C) JavaZOOM 1999-2005.
@@ -32,7 +32,6 @@ import java.util.OptionalInt;
  * Description of the Class
  *
  * @author micah
- * @created December 8, 2001
  */
 public final class Header {
 
@@ -55,27 +54,12 @@ public final class Header {
     /**
      * Description of the Field
      */
-    public final static int STEREO = 0;
-
-    /**
-     * Description of the Field
-     */
     public final static int JOINT_STEREO = 1;
 
     /**
      * Description of the Field
      */
-    public final static int DUAL_CHANNEL = 2;
-
-    /**
-     * Description of the Field
-     */
     public final static int SINGLE_CHANNEL = 3;
-
-    /**
-     * Description of the Field
-     */
-    public final static int FOURTYFOUR_POINT_ONE = 0;
 
     /**
      * Description of the Field
@@ -90,7 +74,7 @@ public final class Header {
     /**
      * Description of the Field
      */
-    public final static int bitrates[][][] = {
+    public final static int[][][] bitrates = {
             {{0
                     /*
                      *  free format
@@ -129,36 +113,6 @@ public final class Header {
                             96000, 112000, 128000, 160000, 192000, 224000, 256000, 320000, 0}}
     };
 
-    /**
-     * Description of the Field
-     */
-    public final static String bitrate_str[][][] = {
-            {{"free format", "32 kbit/s", "48 kbit/s", "56 kbit/s", "64 kbit/s",
-                    "80 kbit/s", "96 kbit/s", "112 kbit/s", "128 kbit/s", "144 kbit/s",
-                    "160 kbit/s", "176 kbit/s", "192 kbit/s", "224 kbit/s", "256 kbit/s",
-                    "forbidden"},
-                    {"free format", "8 kbit/s", "16 kbit/s", "24 kbit/s", "32 kbit/s",
-                            "40 kbit/s", "48 kbit/s", "56 kbit/s", "64 kbit/s", "80 kbit/s",
-                            "96 kbit/s", "112 kbit/s", "128 kbit/s", "144 kbit/s", "160 kbit/s",
-                            "forbidden"},
-                    {"free format", "8 kbit/s", "16 kbit/s", "24 kbit/s", "32 kbit/s",
-                            "40 kbit/s", "48 kbit/s", "56 kbit/s", "64 kbit/s", "80 kbit/s",
-                            "96 kbit/s", "112 kbit/s", "128 kbit/s", "144 kbit/s", "160 kbit/s",
-                            "forbidden"}},
-            {{"free format", "32 kbit/s", "64 kbit/s", "96 kbit/s", "128 kbit/s",
-                    "160 kbit/s", "192 kbit/s", "224 kbit/s", "256 kbit/s", "288 kbit/s",
-                    "320 kbit/s", "352 kbit/s", "384 kbit/s", "416 kbit/s", "448 kbit/s",
-                    "forbidden"},
-                    {"free format", "32 kbit/s", "48 kbit/s", "56 kbit/s", "64 kbit/s",
-                            "80 kbit/s", "96 kbit/s", "112 kbit/s", "128 kbit/s", "160 kbit/s",
-                            "192 kbit/s", "224 kbit/s", "256 kbit/s", "320 kbit/s", "384 kbit/s",
-                            "forbidden"},
-                    {"free format", "32 kbit/s", "40 kbit/s", "48 kbit/s", "56 kbit/s",
-                            "64 kbit/s", "80 kbit/s", "96 kbit/s", "112 kbit/s", "128 kbit/s",
-                            "160 kbit/s", "192 kbit/s", "224 kbit/s", "256 kbit/s", "320 kbit/s",
-                            "forbidden"}}
-    };
-
 
     public static int framesize;
     public static int nSlots;
@@ -166,16 +120,11 @@ public final class Header {
     private static int h_version;
     private static int h_mode;
     private static int h_sample_frequency;
-    private static int h_number_of_subbands, h_intensity_stereo_bound;
     static byte syncmode = BitStream.INITIAL_SYNC;
 
 
     public int version() {
         return h_version;
-    }
-
-    public int bitrate_index() {
-        return h_bitrate_index;
     }
 
     public int sample_frequency() {
@@ -190,65 +139,12 @@ public final class Header {
         return h_mode;
     }
 
-    public boolean padding() {
-        return (h_padding_bit != 0);
-    }
-
     public int slots() {
         return nSlots;
     }
 
     public int mode_extension() {
         return h_mode_extension;
-    }
-
-    public String layer_string() {
-        return "III";
-    }
-
-    public String bitrate_string() {
-        return bitrate_str[h_version][h_layer - 1][h_bitrate_index];
-    }
-
-    public String sample_frequency_string() {
-        switch (frequencies[h_version][h_sample_frequency]) {
-            case 16000:
-                return "16 kHz";
-            case 22050:
-                return "22.05 kHz";
-            case 24000:
-                return "24 kHz";
-            case 32000:
-                return "32 kHz";
-            case 44100:
-                return "44.1 kHz";
-            case 48000:
-                return "48 kHz";
-        }
-        return "not set";
-    }
-
-    public String mode_string() {
-        switch (h_mode) {
-            case STEREO:
-                return "Stereo";
-            case JOINT_STEREO:
-                return "Joint stereo";
-            case DUAL_CHANNEL:
-                return "Dual channel";
-            case SINGLE_CHANNEL:
-                return "Single channel";
-        }
-
-        return "not set";
-    }
-
-    public int number_of_subbands() {
-        return h_number_of_subbands;
-    }
-
-    public int intensity_stereo_bound() {
-        return h_intensity_stereo_bound;
     }
 
     /**
@@ -546,11 +442,13 @@ public final class Header {
             h_padding_bit = (headerstring >>> 9) & 1;
             h_mode = ((headerstring >>> 6) & 3);
             h_mode_extension = (headerstring >>> 4) & 3;
+            int h_intensity_stereo_bound;
             if (h_mode == JOINT_STEREO) {
                 h_intensity_stereo_bound = (h_mode_extension << 2) + 4;
             } else {
                 h_intensity_stereo_bound = 0;
             }
+            int h_number_of_subbands;
             if (h_layer == 1) {
                 h_number_of_subbands = 32;
             } else {
