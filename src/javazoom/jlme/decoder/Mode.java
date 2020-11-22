6856fe502bb48e7d5d0ff00ec790c80bcc89f0af
Joan Andrés <andres6936@live.com>
2020-11-22 17:27:11 -0500

    Added documentation to term "stereo"

--- src/javazoom/jlme/decoder/Mode.java
+++ src/javazoom/jlme/decoder/Mode.java
@@ -5,6 +5,12 @@ package javazoom.jlme.decoder;
  * in Layer III it is intensity_stereo and/or ms_stereo.
  */
 public enum Mode {
+
+    /**
+     * Mode, where two audio channels which form a stereo pair (left and right)
+     * are encoded within one bit stream. The coding process is the same as for
+     * the dual channel mode.
+     */
     STEREO,
     JOIN_STEREO,
     DUAL_CHANNEL,
