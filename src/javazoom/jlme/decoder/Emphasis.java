2d23340938d334b9d43099eb2bb5499898664f09
Joan Andrés <andres6936@live.com>
2020-11-22 17:24:33 -0500

    Added documentation to term "reserved"

--- src/javazoom/jlme/decoder/Emphasis.java
+++ src/javazoom/jlme/decoder/Emphasis.java
@@ -3,6 +3,10 @@ package javazoom.jlme.decoder;
 /**
  * Filtering applied to an audio signal before storage or transmission to
  * improve the signal-to-noise ratio at high frequencies.
+ *
+ * @implNote The term "reserved" when used in the clauses defining the coded
+ * bit stream indicates that the value may be used in the future for ISO/IEC
+ * defined extensions.
  */
 public enum Emphasis {
     NO_EMPHASIS,
