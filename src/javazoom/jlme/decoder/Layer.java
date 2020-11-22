8da87ecdb8b9e8745997ff3f5a388972dcff57fb
Joan Andrés <andres6936@live.com>
2020-11-22 17:21:02 -0500

    Added documentation

--- src/javazoom/jlme/decoder/Layer.java
+++ src/javazoom/jlme/decoder/Layer.java
@@ -1,5 +1,9 @@
 package javazoom.jlme.decoder;
 
+/**
+ * One of the levels in the coding hierarchy of the audio system defined in
+ * this part of the CD.
+ */
 public enum Layer {
     LAYER1,
     LAYER2,
