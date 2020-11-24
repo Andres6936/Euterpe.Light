3a5b785fcde3e423ff878b71d9d278ccec01878b
Joan Andrés <andres6936@live.com>
2020-11-23 19:48:11 -0500

    Use of the values assigned

--- src/javazoom/jlme/decoder/Header.java
+++ src/javazoom/jlme/decoder/Header.java
@@ -452,7 +452,9 @@ public final class Header {
             System.out.println("Emphasis: " + getEmphasis(headerstring).name());
 
             layer = getLayerUsed(headerstring);
+            // Convert the value of kHz to hz.
             bitrate = getBitRateIndex(headerstring) * 1_000;
+            // Convert the value of kHz to hz.
             sampleFrequency = (int) getSamplingFrequency(headerstring) * 1_000;
 
             if (syncmode == BitStream.INITIAL_SYNC) {
@@ -548,7 +550,7 @@ public final class Header {
      */
     private void calFrameSize() {
         if (h_version == MPEG1) {
-            framesize = (144 * bitrates[h_version][h_layer - 1][h_bitrate_index]) / frequencies[h_version][h_sample_frequency];
+            framesize = (144 * bitrate) / sampleFrequency;
         } else {
             framesize = (144 * bitrates[h_version][h_layer - 1][h_bitrate_index]) / (frequencies[h_version][h_sample_frequency] << 1);
         }
