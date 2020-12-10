fd68afc4a5670380f214be8acf8ae5a6ff711c53
Joan Andrés <andres6936@live.com>
2020-12-10 12:33:28 -0500

    Update documentation

--- src/javazoom/jlme/decoder/SideInformation.java
+++ src/javazoom/jlme/decoder/SideInformation.java
@@ -1,10 +1,14 @@
 package javazoom.jlme.decoder;
 
 /**
- * The side information is 17 bytes for mono, 32 bytes otherwise. There’s
- * lots of information in the side info. Most of the bits describe how the
- * main data should be parsed, but there are also some parameters saved
- * here used by other parts of the decoder.
+ * The side information part of the frame consists of information needed to
+ * decode the main data. The size depends on the encoded channel mode.
+ * If it is a single channel bitstream the size will be 17 bytes, if not,
+ * 32 bytes are allocated.
+ * <p>
+ * There’s lots of information in the side info. Most of the bits describe
+ * how the main data should be parsed, but there are also some parameters
+ * saved here used by other parts of the decoder.
  */
 public class SideInformation {
     public int main_data_begin;
